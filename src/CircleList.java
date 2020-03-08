583adf4294388af80c154a04cf842c863e474f05
Lcarrot <vip.kfkf2013@bk.ru>
2020-03-09 00:25:22 +0300

    +List

--- src/CircleList.java
+++ src/CircleList.java
@@ -1,15 +1,17 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class CircleList {
+
+    private LinkedList<Participant> participants;
 
     private class Participant {
 
         private String name;
         private boolean sex;
-        private Participant next;
 
         private Participant(String name, boolean sex) {
             this.name = name;
@@ -18,17 +20,16 @@ public class CircleList {
     }
 
     public CircleList(String filename) {
+        participants = null;
         try {
             FileReader reader = new FileReader(filename);
             Scanner sc = new Scanner(reader);
-            int i = 0;
-            Participant[] participants = new Participant[1000];
             while (sc.hasNextLine()) {
-                participants[i] = new Participant(sc.next(), sc.next().trim().equals("man")); //нужно реализовать через List, пока сырой код
+                participants.add(new Participant(sc.next(), sc.next().trim().equals("man")));
             }
             reader.close();
         } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
