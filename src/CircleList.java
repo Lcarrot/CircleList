e1916bac50d145a90f9221a4fbbb36705c8d6108
Lcarrot <vip.kfkf2013@bk.ru>
2020-03-16 20:03:46 +0300

    TODO method sort and last

--- src/CircleList.java
+++ src/CircleList.java
@@ -1,17 +1,19 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CircleList {
 
-    ArrayList<Participant> participants;
+    private Participant head;
+    private Participant last;
+    private int size;
 
     private class Participant {
 
         private String name;
         private boolean sex;
+        private Participant next;
 
         private Participant(String name, boolean sex) {
             this.name = name;
@@ -19,69 +21,102 @@ public class CircleList {
         }
     }
 
+    public CircleList(Participant head, Participant last, int size) {
+        this.head = head;
+        this.last = last;
+        this.size = size;
+    }
+
     public CircleList(String filename) throws IOException {
         try {
             FileReader reader = new FileReader(filename);
             Scanner sc = new Scanner(reader);
+            size = 0;
+            Participant participant;
             while (sc.hasNextLine()) {
-                participants.add(new Participant(sc.next(), sc.next().equalsIgnoreCase("man")));
+                participant = (new Participant(sc.next(), sc.next().equalsIgnoreCase("man")));
+                if (size == 0) {
+                    head = participant;
+                    size++;
+                }
+                last = participant;
+                participant = participant.next;
+                size++;
             }
+            assert last != null;
+            last.next = head;
             reader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    private CircleList(ArrayList<Participant> participants) {
-        this.participants = participants;
-    }
-
     public void show() {
-        for (Participant participant: participants) {
+        Participant participant = head;
+        for (int i = 0; i < size; i++) {
             System.out.print(participant + ", ");
+            participant = participant.next;
         }
     }
 
     public void insert(Participant participant) {
-        participants.add(participant);
+        if (head == null) {
+            head = participant;
+            last = participant;
+        }
+        last.next = participant;
+        last = participant;
+        last.next = head;
     }
 
     public void delete(String name) {
-        int i = 0;
-        for (Participant p: participants) {
-            if (p.name.equals(name)) {
-                participants.remove(i);
-                break;
+        Participant participant = head;
+        boolean flag = true;
+        if (head.name.equals(name)) {
+            flag = false;
+            last.next = head.next;
+            head = head.next;
+        }
+        for (int i = 0; i < size && flag; i++) {
+            if (participant.next.name.equals(name)) {
+                participant.next = participant.next.next;
             }
-            i++;
         }
     }
 
     public void sort(String name) {
-        int start = participants.indexOf(name);
-        {
-            //нужно спросить как сортировать, через компаратор или тот же список, но где первый это indexOf
-        }
+        // TODO: 16.03.2020
     }
 
     public void last(int k) {
-
+        // TODO: 16.03.2020
     }
 
     public CircleList[] gender() {
-        CircleList[] people = new CircleList[2];
-        ArrayList men = new ArrayList<>();
-        ArrayList<Participant> women = new ArrayList<>();
-        for (Participant participant : participants) {
-            if (participant.sex) {
-                men.add(participant);
+        Participant p = head;
+        Participant[] people = new Participant[]{new Participant(null, true), new Participant(null, false)};
+        Participant[] first  = new Participant[]{new Participant(null, true), new Participant(null, false)};
+        Participant[] last   = new Participant[]{new Participant(null, true), new Participant(null, false)};
+        int[] size = new int[]{0, 0};
+        int sex;
+        for (int i = 0; i < this.size; i++) {
+            if (p.sex) {
+                sex = 0;
+            } else {
+                sex = 1;
             }
-            else {
-                women.add(participant);
+            if (people[sex].name == null) {
+                first[sex].name = p.name;
+                people[sex] = first[sex];
+                last[sex] = first[sex];
+                size[sex]++;
+            } else {
+                people[sex].next.name = p.name;
+                people[sex].next.sex = p.sex;
+                people[sex] = people[sex].next;
+                last[sex] = people[sex];
             }
         }
-        people[0] = new CircleList(men);
-        people[1] = new CircleList(women);
-        return people;
+        return new CircleList[]{new CircleList(first[0],last[0],size[0]), new CircleList(first[1],last[1],size[1])};
     }
 }
